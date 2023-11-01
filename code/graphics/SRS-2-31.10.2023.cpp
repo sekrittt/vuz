0e1b245d57fcc651611b1142a062b4864ea9d9a8
sekrittt <61773395+sekrittt@users.noreply.github.com>
2023-11-01 11:14:30 +0500

    .

--- code/graphics/SRS-2-31.10.2023.cpp
+++ code/graphics/SRS-2-31.10.2023.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-int x;
-int y;
-int Sn(int);
-int Cn(int n)
+float x;
+float y;
+float Sn(float);
+float Cn(float n)
 {
 	if (n <= 1) return x;
 	return Cn(n-1)*x - Sn(n-1)*y;
 }
 
-int Sn(int n)
+float Sn(float n)
 {
 	if (n <= 1) return y;
 	return Sn(n-1)*x + Cn(n-1)*y;
@@ -20,10 +20,30 @@ int Sn(int n)
 
 int main()
 {
-	int n;
+	float s1, c1, s, c, n;
 	cout << "Enter x(x^2 < 1) and n: ";
 	cin >> x >> n;
 	y = sqrt(1 - x*x);
-	cout << Cn(n) << endl << Sn(n) << endl;;
+	s1 = y;
+	c1 = x;
+	cout << 1 << ": s=" << s1 << " c=" << c1 << " Sn=" << Sn(1) << " Cn=" << Cn(1) << endl;
+	for (int i = 2; i <= n; i++)
+	{
+		s = s1 * x + c1 * y;
+		c = c1 * x - s1 * y;
+		s1 = s;
+		c1 = c;
+		cout << i << ": s=" << s << " c=" << c << " Sn=" << Sn(i) << " Cn=" << Cn(i) << endl;
+	}
 	return 0;
 }
+/*
+Test 1:
+	Enter x(x^2 < 1) and n: 0.5 6
+	1: 0.866025 0.5 0.866025 0.5
+	2: 0.866025 -0.5 0.866025 -0.5
+	3: 0 -1 0 -1
+	4: -0.866025 -0.5 -0.866025 -0.5
+	5: -0.866025 0.5 -0.866025 0.5
+	6: 0 1 0 1
+*/
