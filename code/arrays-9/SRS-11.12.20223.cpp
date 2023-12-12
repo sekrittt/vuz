32db91c53c6ca19d13c3073d4de3dfd492487f7c
Sekrittt Sekrittt <61773395+sekrittt@users.noreply.github.com>
2023-12-12 22:00:44 +0500

    Update SRS-11.12.20223.cpp

--- code/arrays-9/SRS-11.12.20223.cpp
+++ code/arrays-9/SRS-11.12.20223.cpp
@@ -5,39 +5,6 @@
 
 using namespace std;
 
-void array_sort(int *M, int n)
-{
-    int left = 0;      // левая граница
-    int right = n - 1; // правая граница
-    int j, t, k = n;   // k-фиксирование индекса посл-его обмена
-    do
-    { // Прямой проход «Пузырьком» от левой границы до правой
-        for (j = left; j < right; j++)
-        {
-            if (*(M + j) > *(M + j + 1))
-            {
-                t = *(M + j);
-                *(M + j) = *(M + j + 1);
-                *(M + j + 1) = t;
-                k = j;
-            }
-        }
-        right = k; // правая  граница
-                   // Обратный проход «Пузырьком» от правой границы до левой
-        for (j = right; j > left; j--)
-        {
-            if (*(M + j - 1) > *(M + j))
-            {
-                t = *(M + j);
-                *(M + j) = *(M + j - 1);
-                *(M + j - 1) = t;
-                k = j;
-            }
-        }
-        left = k;           // Левая граница
-    } while (left < right); // До тех пор, пока левая граница не станет больше правой границы
-}
-
 void array_print(int *M, int n) // функция для вывода массива
 {                               //
     cout << "{";                //
@@ -60,10 +27,51 @@ void fill_worst(int *M, int n)       // заполнение в худшем
     }                                //
 }
 
+void array_sort(int *M, int n)
+{
+    int left = 0;      // левая граница
+    int right = n - 1; // правая граница
+    int j, t, k = n;   // k-фиксирование индекса посл-его обмена
+    do
+    { // Прямой проход «Пузырьком» от левой границы до правой
+        cout << left << " " << right << " " << *(M + left) << " " << *(M + right) << endl;
+        for (j = left; j < right; j++)
+        {
+            if (*(M + j) > *(M + j + 1))
+            {
+                t = *(M + j);
+                *(M + j) = *(M + j + 1);
+                *(M + j + 1) = t;
+                k = j;
+            }
+        }
+        cout << "После прямого прохода: ";
+        array_print(M, n);
+        right = k; // правая  граница
+                   // Обратный проход «Пузырьком» от правой границы до левой
+        cout << left << " " << right << " " << *(M + left) << " " << *(M + right) << endl;
+        for (j = right; j > left; j--)
+        {
+            if (*(M + j - 1) > *(M + j))
+            {
+                t = *(M + j);
+                *(M + j) = *(M + j - 1);
+                *(M + j - 1) = t;
+                k = j;
+            }
+        }
+        cout << "После обратного прохода: ";
+        array_print(M, n);
+        left = k;           // Левая граница
+    } while (left < right); // До тех пор, пока левая граница не станет больше правой границы
+}
+
 int main()
 {
     srand(time(0));
-    int n = 10;
+    int n;
+    cout << "Please enter n: ";
+    cin >> n;
     int *M = new int[n];
     fill_worst(M, n);
     cout << "Array M = ";
