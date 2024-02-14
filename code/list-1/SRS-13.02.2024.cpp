85f58e4c74f096b71ef73e8c9f79cfbbf6865263
Sekrittt Sekrittt <61773395+sekrittt@users.noreply.github.com>
2024-02-14 21:14:35 +0500

    Update SRS-13.02.2024.cpp

--- code/list-1/SRS-13.02.2024.cpp
+++ code/list-1/SRS-13.02.2024.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
@@ -8,31 +9,36 @@ struct Node
     Node *Next = NULL;
 };
 
+void list_print(Node *one)
+{
+    Node *el = one;
+    while (el != NULL)
+    {
+        cout << "Address of " << (*el).D << " = " << el << " Next element address = " << (*el).Next << endl;
+        el = (*el).Next;
+    }
+}
+
 int main()
 {
+    srand(time(0));
     Node *one = new (Node);
 
     int len;
     cout << "Please enter enter length of list: ";
     cin >> len;
-    cout << "Please enter value of 1 element: ";
-    cin >> (*one).D;
+    (*one).D = rand() % 100;
+    cout << "Address of " << (*one).D << " = " << one << " Next element address = " << (*one).Next << endl;
     Node *prev = one;
     for (int i = 1; i < len; i++)
     {
         Node *t = new (Node);
-        cout << "Please enter value of " << i + 1 << " element: ";
-        cin >> (*t).D;
+        (*t).D = rand() % 100;
         (*prev).Next = t;
         prev = t;
-        // Print elements
-        Node *el = one;
-        while (el != NULL)
-        {
-            cout << "Address of " << (*el).D << " = " << el << " Next element address = " << (*el).Next << endl;
-            el = (*el).Next;
-        }
     }
+
+    list_print(one);
 
     Node *el = one;
     Node *max_el = one;
@@ -47,6 +53,16 @@ int main()
     }
 
     cout << "Max element: " << (*max_el).D << endl;
+
+    // Remove 3 elements after max
+    Node *target_el = max_el;
+    for (int i = 0; i < 4 && target_el != NULL; i++)
+    {
+        target_el = (*target_el).Next;
+    }
+    (*max_el).Next = target_el;
+
+    list_print(one);
 
     return 0;
 }
