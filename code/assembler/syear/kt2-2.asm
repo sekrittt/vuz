4f16acdc2e48892cd9cc62490e9b78c1b0a689ec
Sekrittt Sekrittt <61773395+sekrittt@users.noreply.github.com>
2024-10-11 16:12:10 +0500

    Update kt2-2.asm

--- code/assembler/syear/kt2-2.asm
+++ code/assembler/syear/kt2-2.asm
@@ -5,9 +5,19 @@
 
 .data
     msg db "Yes"
+    msg1 db 'Hello'
+    msg2 db 'World'
+    msg3 db '!!!!!!!!!!'
+
+    msg_curr dd ?
+    msg_curr_len dw 3
+    msg_len dw 3
+    msg1_len dw 5
+    msg2_len dw 5
+    msg3_len dw 10
 
 .code
-include libs\drawRect.asm
+include libs\io.asm
 clearScreen proc
     mov ax, 0003h
     int 10h
@@ -16,6 +26,12 @@ clearScreen endp
 start proc
     mov ax, @data
     mov ds, ax
+
+    mov word ptr [msg_curr], offset msg
+    push ax
+    mov ax, msg_len
+    mov msg_curr_len, ax
+    pop ax
 
     call clearScreen
     ; Init
@@ -31,7 +47,7 @@ start proc
     int 33h
     ; Setup custom handler
     mov ax, 000Ch
-    mov cx, 00000001b
+    mov cx, 00101011b
     push cs
     pop es
     mov dx, offset mouse_handler
@@ -43,12 +59,14 @@ start proc
 start endp
 
 mouse_handler proc far ; dx, cx - coords
+    push bx
     call clearScreen
     mov ax, 0001h
     int 33h
 
     push ds
     pop es
+
 
     mov ax, dx
     mov bl, 8
@@ -63,13 +81,101 @@ mouse_handler proc far ; dx, cx - coords
     mov ah, 13h
     mov al, 0
     mov bl, 00001111b
-    mov cx, 3
-    mov bp, offset msg
+    mov cx, word ptr [msg_curr_len]
+    mov bp, word ptr [msg_curr]
     int 10h
 
 
+    ; Sound Effect
+    pop bx
+    cmp bx, 00000001b
+    je left_click
+    cmp bx, 00000010b
+    je right_click
+    cmp bx, 00000100b
+    je middle_click
+    jmp m_Exit
+
+
+;ОБРАБОТКА ЛЕВОГО КЛИКА МЫШИ
+left_click:
+    mov word ptr [msg_curr], offset msg1
+    push ax
+    mov ax, msg1_len
+    mov msg_curr_len, ax
+    pop ax
+    jmp m_Exit
+
+;ОБРАБОТКА ПРАВОГО КЛИКА МЫШИ
+right_click:
+    mov word ptr [msg_curr], offset msg2
+    push ax
+    mov ax, msg2_len
+    mov msg_curr_len, ax
+    pop ax
+    jmp m_Exit
+
+;ОБРАБОТКА СРЕДНЕГО КЛИКА МЫШИ
+middle_click:
+    mov word ptr [msg_curr], offset msg3
+    push ax
+    mov ax, msg3_len
+    mov msg_curr_len, ax
+    pop ax
+    mov di,1000 ; 1000 Герц    
+	mov cx,18   ; 1 секунда    
+	call sound
+    jmp m_Exit
+
+m_Exit:
     retf
 mouse_handler endp
+
+sound proc
+    mov al,0B6h    
+    out 43h,al    
+    mov dx, 14h   ; freq=1331000 Hz    
+    mov ax, 4f38h    
+    div di    
+    out 42h,al    
+    mov al,ah    
+    out 42h,al    
+    in al,61h    
+    mov ah,al    
+    mov al, 61h    
+    or al,3  ; speaker on    
+    out 61h,al ; задержка звука    
+    push es    
+    push ax    
+    mov  ax,0    
+    mov  es,ax
+
+wt0:   
+    mov  ax,es:[46Ch] ; счётчик системного таймера (18 тиков в секунду)
+
+wt1:   
+    push ax    
+    mov  ah,1 ; выход, если нажата клавиша    
+    int 16h    
+    jnz ex    
+    pop  ax    
+    cmp  ax,es:[46Ch]    
+    jz   wt1    
+    loop wt0    
+    pop  ax    
+    pop  es    
+    mov al,ah    
+    out 61h,al ; speaker off         
+    ret
+ex:
+    mov al, 61h    
+    out 61h,al ; speaker off    
+    mov ah,0    
+    int 16h  
+    ret
+sound endp
+
+
 
 exit proc
     ;
