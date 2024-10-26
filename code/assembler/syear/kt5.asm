c5e5d580b32fa715753e1af800312be1b53daede
Sekrittt Sekrittt <61773395+sekrittt@users.noreply.github.com>
2024-10-26 13:57:33 +0500

    .

--- code/assembler/syear/kt5.asm
+++ code/assembler/syear/kt5.asm
@@ -1,26 +1,29 @@
-.model tiny
+.model small
 .386
 .stack 100h
 
 .data
     errormes db 'Error','$'
-    x dd 0
-    n dd 0
-    outp dd 1.0
-    i dd 1.0
-    chisl dd 1.0
-    znam  dd 1.0
-    tmp dd 1.0
+    x dt 0
+    x1 dt 0
+    n dt 0
+    i dt 1.0
+    result dt 0
+    minusOne dt -1.0
+    lastSign dt 1.0
+
+    tmp dt 0
+
     ten equ word ptr [bp-2] ; какой-то регистр для вывода
     temp equ word ptr [bp-4] ; еще какой-то регистр для вывода
 
-    message1 db "Ряд Тейлора от ln(1-x)", '$'
-    message2 db "Введите N = ", '$'
-    message3 db "Веедите X = ", '$'
+    welcomeMsg db "Ряд Тейлора от ln(1+x)", '$'
+    enterNMsg db "Введите N = ", '$'
+    enterXMsg db "Веедите X = ", '$'
     message4 db "Ряд: ", '$'
     message5 db " + ", '$'
     message6 db "/", '$'
-    message7 db " = -", '$'
+    message7 db " = ", '$'
     message8 db "  ", '$'
     msgContinueQuestion db "Продолжить? (Y/N) ", '$'
     msgIncorectNumber db "Неправильное число!", "$"
@@ -28,6 +31,7 @@
     nStr db 254, 0, 254 dup("$")
     xStr db 254, 0, 254 dup("$")
     continueQuestionAnswer db 254, 0, 254 dup("$")
+    controlReg dw ?
 
 .code
 
@@ -43,43 +47,42 @@ main proc
     mov ax, @data
     mov ds, ax
 
+    finit
+
 main_loop:
     call clearscreen
 
-    mov ax, offset message1
+    mov ax, offset welcomeMsg
     call println
 
-    mov ax, offset message2
+    mov ax, offset enterNMsg
     mov bx, offset nStr
     call input
 
     mov si, offset nStr + 2
     call string_to_float
-    fst n
+    fstp n
 
 
-    mov ax, offset message3
+    mov ax, offset enterXMsg
     mov bx, offset xStr
     call input
 
     mov si, offset xStr + 2
     call string_to_float
-    fchs
-    fst x
-
+    fstp x
+    fld x
+    fstp x1
 
     mov ax, offset message4
     call print
-
-    fld x
-    fst chisl
 
     call tayl
 
     mov ax, offset message7
     call print
 
-    fld outp
+    fld result
 
     call print_float
 
@@ -100,64 +103,85 @@ main_exit:
 main endp
 
 tayl proc
+    push ax
     push cx
     xor cx, cx
-    taylcicle:
-        cmp cx, 0
-        je __skip
-        mov ax, offset message5
-        call print
-    __skip:
-        fld chisl
-        call print_float
+    ; Устанавливаем значения по умолчанию
+    fld x
+    fstp result
+    fld1
+    fstp i
+    fld1
+    fstp lastSign
+    ;
+    fld result
+    call print_float
+    mov ax, offset message5
+    call print
 
-        mov ax, offset message6
-        call print
+    ; Получаем следующую степень x
+    fld x
+    fld x1
+    fmul st(0), st(1)
+    fstp x
 
-        fld znam
-        call print_float
+    tayl_loop:
 
-        fld znam
-        fld chisl
+        fld minusOne
+        fld lastSign
+        fmul st(0), st(1) ; Формируем новый знак
+        fstp lastSign
 
-        fdiv st(0), st(1)
-        fst tmp
+        fld i ;
+        fld1 ; Загружаем единицу в стек
+        fadd st(0), st(1) ; Добавляем единицу к текущему значению i
+        fstp i ; Сохраняем текущее i
+
+        ; fld i
+        ; call print_float
+        ; call print_newline
+        ; fld x
+        ; call print_float
+        ; call print_newline
+
+        fld i ; Загружаем текущий i
+        fld x ; Загружаем текущий x
+        fdiv st(0), st(1) ; Делим x на i
+        fstp tmp
 
         fld tmp
+        fld lastSign ; Загружаем текущий знак
+        fmul st(0), st(1) ;
+        fstp tmp
+        fld tmp
+        call print_float
 
-        fld outp
+        fld result ; Загружаем предыдущий промежуточный реультат
+        fld tmp
+        fadd st(0), st(1) ; Складываем
+        fstp result ; Сохраняем промежуточный результат
 
-        fsub st(0), st(1)
-        fst outp
+        ; Получаем следующую степень x
+        fld x
+        fld x1
+        fmul st(0), st(1)
+        fstp x
 
+        ; Проверяем закончили ли мы
         fld i
-        fcom n
+        fld n
+        fcom
         fstsw ax
         sahf
-        mov cx, 1
-        jb taylinc
+        je tayl_exit
+        jb tayl_exit
+        mov ax, offset message5
+        call print
+        jmp tayl_loop
+    tayl_exit:
         pop cx
+        pop ax
         ret
-    taylinc:
-        fld chisl
-        fld x
-        fmul
-        fchs
-        fst chisl
-
-        fld1
-        fld znam
-        fadd
-        fst znam
-
-        fld i
-        fld1
-        fadd
-        fst i
-        mov cx, 1
-        jmp taylcicle
-    pop cx
-    ret
 tayl endp
 
 exit proc
